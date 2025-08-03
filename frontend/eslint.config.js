ce638eec31f880b5bd453d36ebccebddd64e1e13
juvenigi <juvenigi@gmail.com>
2025-08-03 23:59:47 +0200

    eslint: make life harder

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import * as reactDom from "typescript-eslint";
+import * as reactX from "typescript-eslint";
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -11,7 +13,13 @@ export default tseslint.config([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      // Alternatively, use this for stricter rules
+      tseslint.configs.strictTypeChecked,
+      // Optionally, add this for stylistic rules
+      ...tseslint.configs.stylisticTypeChecked,
+      reactX.configs['recommended-typescript'],
+      // Enable lint rules for React DOM
+      reactDom.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
